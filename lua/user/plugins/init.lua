71b36e03ea297f583ede3ffaa06edded9167a22e
ttutko <ttutko@outlook.com>
2023-08-20 01:41:58 -0400

    Added treesitter

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -16,5 +16,19 @@ return {
   },
   {
     "williamboman/mason.nvim"
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "c_sharp", "python" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
   }
 }
