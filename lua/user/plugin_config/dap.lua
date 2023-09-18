712fac2e58d239530ee67c7b054446eb84d67ffc
Tom <tom@popvm.local>
2023-09-18 14:56:53 -0400

    Setup dap-ui to launch when debugging starts

--- lua/user/plugin_config/dap.lua
+++ lua/user/plugin_config/dap.lua
@@ -9,7 +9,17 @@ dap.adapters.coreclr = {
 
 dapui.setup()
 
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
 
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
 -- dap.configuration.cs = {
 --   {
 --     type = "coreclr",
